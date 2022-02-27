5f14601eb00bfd2a19dcfb7d34c0b162517c7d46
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-02-27 18:29:51 +0900

    feat: stream 기본

---
+++ src/streamPractice/App.java
@@ -0,0 +1,37 @@
+package streamPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("java");
+        names.add("jpa");
+        names.add("spring");
+        names.add("mysql");
+
+        // Lazy 하기 때문에 실행 안됨
+        names.stream()
+                .map(s->{
+                    System.out.println(s.toUpperCase());
+                    return s.toUpperCase();
+                });
+
+        System.out.println();
+        System.out.println("==========");
+
+        names.parallelStream().map(s -> {
+            System.out.println(s.toUpperCase() + ' ' + Thread.currentThread().getName());
+            return s.toUpperCase();
+        }).collect(Collectors.toList());
+
+        System.out.println();
+
+        names.stream().map(s -> {
+            System.out.println(s.toUpperCase() + ' ' + Thread.currentThread().getName());
+            return s.toUpperCase();
+        }).collect(Collectors.toList());
+    }
+}
